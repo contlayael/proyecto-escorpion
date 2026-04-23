fbfdf7afe3c671d519ee6980152cd121a55fdee3
yaelcontla <yael.contla@hotmail.com>
2026-04-22 22:29:07 -0600

    feat: integración de PostgreSQL con TypeORM, conexión a BD y creación de entidad Product

--- backend-textiles/src/app.module.ts
+++ backend-textiles/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // Usuario por defecto
+      password: 'greenevilday15', // LA QUE PUSISTE AL INSTALAR
+      database: 'db_textiles',
+      autoLoadEntities: true, // Esto cargará nuestras clases automáticamente
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // ¡IMPORTANTE! Crea las tablas automáticamente (solo en desarrollo)
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
