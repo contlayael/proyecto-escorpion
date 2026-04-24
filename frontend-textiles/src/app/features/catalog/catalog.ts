e7fd8c07b872f86776188bbfdb6c25076005f33b
yaelcontla <yael.contla@hotmail.com>
2026-04-24 00:09:03 -0600

    feat: integración de Tailwind CSS y renderizado dinámico de productos con Signals ready

--- frontend-textiles/src/app/features/catalog/catalog.ts
+++ frontend-textiles/src/app/features/catalog/catalog.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,20 +12,27 @@ export class Catalog implements OnInit { // <-- Prometemos usar OnInit
   products: any[] = [];
   isLoading = true;
   private http = inject(HttpClient);
+  private cdr = inject(ChangeDetectorRef);
 
   // <-- AQUÍ ESTÁ LA FUNCIÓN QUE CUMPLE LA PROMESA
   ngOnInit() {
+    // 1. Generamos un número único basado en la hora actual
+    const cacheBuster = new Date().getTime();
     console.log('Iniciando petición al backend...'); 
     
-    this.http.get<any[]>('http://localhost:3000/products').subscribe({
+    // 2. Lo inyectamos en la URL (?cb=123456789)
+    this.http.get<any[]>(`http://localhost:3000/products?cb=${cacheBuster}`).subscribe({
       next: (data) => {
         console.log('¡Éxito! Datos recibidos:', data);
         this.products = data;
         this.isLoading = false;
+        // <-- EL CODAZO MÁGICO: Obliga al HTML a reaccionar y dibujarse
+        this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Error conectando al backend:', err);
         this.isLoading = false;
+        this.cdr.detectChanges();// También avisamos si hay error
       }
     });
   }
