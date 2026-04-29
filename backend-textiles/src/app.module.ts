71d1e46751fd77cee6bfeb7eed3fde2f1b7f4170
yaelcontla <yael.contla@hotmail.com>
2026-04-28 21:27:32 -0600

    chore: configurar DB para leer variables de entorno en producción

--- backend-textiles/src/app.module.ts
+++ backend-textiles/src/app.module.ts
@@ -5,18 +5,29 @@ import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    /*TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
       username: 'postgres', // Usuario por defecto
       password: 'greenevilday15', // LA QUE PUSISTE AL INSTALAR
       database: 'db_textiles',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/***.entity{.ts,.js}'],
       //entities: [Product],
       //entities: [__dirname + '/products/entities/*.entity{.ts,.js}'],
-      autoLoadEntities: true, // Esto cargará nuestras clases automáticamente
+      /*autoLoadEntities: true, // Esto cargará nuestras clases automáticamente
       synchronize: true, // ¡IMPORTANTE! Crea las tablas automáticamente (solo en desarrollo)
+    }),*/
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // Usará la URL de la nube si existe, si no, intentará conectarse en local
+      url:
+        process.env.DATABASE_URL ||
+        'postgresql://postgres:greenevilday15@localhost:5432/db_textiles',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Necesario para la nube
     }),
     ProductsModule,
     OrdersModule,
